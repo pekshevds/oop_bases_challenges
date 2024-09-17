ae6a6c2b38c121fd6bbba2a64f218bebdb4460f4
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-17 08:39:44 +0300

    a_credit_bank_account.py

--- level_3/a_credit_bank_account.py
+++ level_3/a_credit_bank_account.py
@@ -8,10 +8,8 @@
     4. Создать экземпляр класс CreditAccount и вызвать у него каждый из возможных методов.
 """
 
-# код писать тут
 
-
-class CreditAccount:
+class BankAccount:
     def __init__(self, owner_full_name: str, balance: float):
         self.owner_full_name = owner_full_name
         self.balance = balance
@@ -22,10 +20,20 @@ class CreditAccount:
     def decrease_balance(self, amount: float):
         self.balance -= amount
 
+
+class CreditAccount(BankAccount):
     def is_eligible_for_credit(self):
         return self.balance > 1000
 
 
-if __name__ == '__main__':
-    pass  # код писать тут
-
+if __name__ == "__main__":
+    ba1 = BankAccount("Vasya", 1000.99)
+    ba1.increase_balance(548.98)
+    ba1.decrease_balance(125.54)
+    print(f"balance: {ba1.balance}")
+    ca1 = CreditAccount("Petya", 465.88)
+    ca1.increase_balance(1110.30)
+    ca1.decrease_balance(125.00)
+    print(
+        f"balance: {ca1.balance}, is_eligible_for_credit: {ca1.is_eligible_for_credit()}"
+    )
