c2dec5133fca9182aa3f6be5eb2a4fa73d2e9a66
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-10-12 10:44:56 +0300

    level_7\f_students_group.py

--- level_7/f_students_group.py
+++ level_7/f_students_group.py
@@ -15,8 +15,11 @@ class StudentGroup:
         self.group_number = group_number
         self.grades = grades
 
+    def __add__(self, other):
+        return sum([sum(self.grades), sum(other.grades)])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     first_group = StudentGroup(group_number=1, grades=[1, 4, 6, 3])
     second_group = StudentGroup(group_number=2, grades=[6, 3, 7, 3, 2])
     print(first_group + second_group)
