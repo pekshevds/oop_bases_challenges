910631fb2005eb3e0086f9edcf8f83989d22f23e
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-10-12 10:29:59 +0300

    level_7\b_bank_account.py

--- level_7/b_bank_account.py
+++ level_7/b_bank_account.py
@@ -17,8 +17,15 @@ class BankAccount:
         self.balance = balance
 
     def decrease_balance(self, amount: float):
-        pass  # писать код тут
+        balance = self.balance - amount
+        if balance < self.min_balance:
+            raise ValueError(
+                f"the balance must be more than -100, you tried set {balance}"
+            )
+        self.balance = balance
 
 
-if __name__ == '__main__':
-    pass  # писать код тут
+if __name__ == "__main__":
+    bc1 = BankAccount("denis", 1000.99)
+    bc1.decrease_balance(399.49)
+    bc1.decrease_balance(999.99)
